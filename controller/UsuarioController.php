51d7c0f6384eecc6dd6c96493902703a72444bc1
GABRIELI MORALES TABORDA <03657851054@senacrs.edu.br>
2025-04-10 11:04:25 -0300

    first commit

--- controller/UsuarioController.php
+++ controller/UsuarioController.php
@@ -8,8 +8,8 @@
 
             $user = $_POST['usuario'];
             $senha = $_POST['senha'];
-            //$salt = md5($senha);
-            //$senha = hash('sha256', $salt);
+            $salt = md5($senha);
+            $senha = hash('sha256', $senha);
             $usuario->logar($user, $senha);    
         }
 
